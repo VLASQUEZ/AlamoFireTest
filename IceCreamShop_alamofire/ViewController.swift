bc7140950f49620502343ab99d09d82244fab6d0
Andres Velasquez <vlasquez53069@gmail.com>
2018-02-27 12:52:09 -0500

    Initial Commit

---
+++ IceCreamShop_alamofire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IceCreamShop_alamofire
+//
+//  Created by Andres Velasquez on 27/02/18.
+//  Copyright © 2018 Andres Velasquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
